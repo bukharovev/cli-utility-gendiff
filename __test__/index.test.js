78645855fcecbf3f7946d675dd427712a1338ef3
bukharovev <bukharovev@gmail.com>
2019-03-26 22:07:42 +0300

    add test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -4,7 +4,7 @@ import genDiff from '../src';
 test('diff JSON', () => {
   const before = '__tests__/__fixtures__/before.json';
   const after = '__tests__/__fixtures__/after.json';
-  const result = '__tests__/__fixtures__/expectedJSON.txt';
+  const result = String(fs.readFileSync('__tests__/__fixtures__/expectedJSON.txt'));
   expect(genDiff(before, after)).toBe(result);
 });
 
