d4cb1cb29f7ebb45a309cfdf93d9275ab00210d7
bukharovev <bukharovev@gmail.com>
2019-03-27 02:38:52 +0300

    fix bug

---
+++ __tests__/index.test.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import genDiff from '../src';
+
+test('diff JSON', () => {
+  const before = '__tests__/__fixtures__/before.json';
+  const after = '__tests__/__fixtures__/after.json';
+  const result = String(fs.readFileSync('__tests__/__fixtures__/expectedJSON'));
+  expect(genDiff(before, after)).toBe(result);
+});
--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 import _ from 'lodash';
 import fs from 'fs';
 
+const propertyTypes = [
+  {
+    check: ((obj1, obj2, key) => _.has(obj1, key) && _.has(obj2, key) && obj1[key] !== obj2[key]),
+    process: ((obj1, obj2, key) => `  + ${key}: ${obj2[key]}
+  - ${key}: ${obj1[key]}`),
+  },
+  {
+    check: ((obj1, obj2, key) => _.isEqual(obj1[key], obj2[key])),
+    process: ((obj1, obj2, key) => `    ${key}: ${obj1[key]}`),
+  },
+  {
+    check: ((obj1, obj2, key) => _.isUndefined(obj1[key])),
+    process: ((obj1, obj2, key) => `  + ${key}: ${obj2[key]}`),
+  },
+  {
+    check: ((obj1, obj2, key) => _.isUndefined(obj2[key])),
+    process: ((obj1, obj2, key) => `  - ${key}: ${obj1[key]}`),
+  },
+];
+
+const getPropertyTypes = (obj1, obj2, key) => (
+  propertyTypes.find(({ check }) => check(obj1, obj2, key)));
+
 const genDiff = (path1, path2) => {
-  const obj1 = fs.readFileSync(path1);
-  const obj2 = fs.readFileSync(path2);
+  const obj1 = JSON.parse(fs.readFileSync(path1));
+  const obj2 = JSON.parse(fs.readFileSync(path2));
   const keys = _.union(_.keys(obj1), _.keys(obj2));
-  const result = keys.reduce((acc, key) => {
-    if (_.isEqual(obj1[key], obj2[key])) {
-      acc = [...acc, `${key}: ${obj1[key]}`];
-    } else if (_.has(obj1, key) && _.has(obj2, key)) {
-      acc = [...acc, `+ ${key}: ${obj2[key]}`];
-      acc = [...acc, `- ${key}: ${obj1[key]}`];
-    }
-    if (_.isUndefined(obj1[key])) {
-      acc = [...acc, `+ ${key}: ${obj2[key]}`];
-    }
-    if (_.isUndefined(obj2[key])) {
-      acc = [...acc, `- ${key}: ${obj1[key]}`];
-    }
-    return acc;
-  }, []);
+  const res = keys.map((key) => {
+    const { process } = getPropertyTypes(obj1, obj2, key);
+    return process(obj1, obj2, key);
+  });
+  const result = `{\n${res.join('\n')}\n}`;
   return result;
 };
+
 
 export default genDiff;
