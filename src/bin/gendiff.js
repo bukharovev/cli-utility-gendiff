a6746207f6f6e335898548e59b86f6f22c9cea53
bukharovev <bukharovev@gmail.com>
2019-03-26 22:05:29 +0300

    fix bug

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,11 +2,12 @@
 
 import program from 'commander';
 import { version } from '../../package.json';
+import genDiff from '..';
 
 program
   .version(version)
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
+  .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)))
   .parse(process.argv);
-  
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+import fs from 'fs';
+
+const genDiff = (path1, path2) => {
+  const obj1 = JSON.parse(fs.readFileSync(path1));
+  const obj2 = JSON.parse(fs.readFileSync(path2));
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
+  const result = keys.reduce((acc, key) => {
+    if (_.isEqual(obj1[key], obj2[key])) {
+      acc = [...acc, `${key}: ${obj1[key]}`];
+    } else if (_.has(obj1, key) && _.has(obj2, key)) {
+      acc = [...acc, `+ ${key}: ${obj2[key]}`];
+      acc = [...acc, `- ${key}: ${obj1[key]}`];
+    }
+    if (_.isUndefined(obj1[key])) {
+      acc = [...acc, `+ ${key}: ${obj2[key]}`];
+    }
+    if (_.isUndefined(obj2[key])) {
+      acc = [...acc, `- ${key}: ${obj1[key]}`];
+    }
+    return acc;
+  }, []);
+  return result;
+};
+
+export default genDiff;
