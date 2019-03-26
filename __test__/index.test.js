63cf48de98c6064db4a125879d4e7c7c2d4583fd
bukharovev <bukharovev@gmail.com>
2019-03-27 02:55:28 +0300

    deleted

--- __test__/index.test.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import genDiff from '../src';
-
-test('diff JSON', () => {
-  const before = '__tests__/__fixtures__/before.json';
-  const after = '__tests__/__fixtures__/after.json';
-  const result = fs.readFileSync('__tests__/__fixtures__/expectedJSON.txt');
-  expect(genDiff(before, after)).toBe(result);
-});
