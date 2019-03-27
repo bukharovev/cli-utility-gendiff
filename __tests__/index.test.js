4c27b233e5299bf5bcf982ae1bbde4a8286defa0
bukharovev <bukharovev@gmail.com>
2019-03-27 07:21:43 +0300

    add YAML format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,6 +4,13 @@ import genDiff from '../src';
 test('diff JSON', () => {
   const before = '__tests__/__fixtures__/before.json';
   const after = '__tests__/__fixtures__/after.json';
-  const result = String(fs.readFileSync('__tests__/__fixtures__/expectedJSON.txt'));
+  const result = String(fs.readFileSync('__tests__/__fixtures__/expected.txt'));
+  expect(genDiff(before, after)).toBe(result);
+});
+
+test('diff YAML', () => {
+  const before = '__tests__/__fixtures__/before.yaml';
+  const after = '__tests__/__fixtures__/after.yaml';
+  const result = String(fs.readFileSync('__tests__/__fixtures__/expected.txt'));
   expect(genDiff(before, after)).toBe(result);
 });
