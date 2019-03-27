c7b43df3bf5261ee82e980e9c142833ac3ae109d
bukharovev <bukharovev@gmail.com>
2019-03-27 08:45:32 +0300

    test integration

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,23 +1,10 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-test('diff JSON', () => {
-  const before = '__tests__/__fixtures__/before.json';
-  const after = '__tests__/__fixtures__/after.json';
-  const result = String(fs.readFileSync('__tests__/__fixtures__/expected.txt'));
-  expect(genDiff(before, after)).toBe(result);
-});
-
-test('diff YAML', () => {
-  const before = '__tests__/__fixtures__/before.yaml';
-  const after = '__tests__/__fixtures__/after.yaml';
-  const result = String(fs.readFileSync('__tests__/__fixtures__/expected.txt'));
-  expect(genDiff(before, after)).toBe(result);
-});
-
-test('diff INI', () => {
-  const before = '__tests__/__fixtures__/before.ini';
-  const after = '__tests__/__fixtures__/after.ini';
-  const result = String(fs.readFileSync('__tests__/__fixtures__/expected.txt'));
+const formatsFile = ['json', 'yaml', 'ini'];
+test.each(formatsFile)('%s diff', (format) => {
+  const before = `__tests__/__fixtures__/before.${format}`;
+  const after = `__tests__/__fixtures__/after.${format}`;
+  const result = fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8');
   expect(genDiff(before, after)).toBe(result);
 });
