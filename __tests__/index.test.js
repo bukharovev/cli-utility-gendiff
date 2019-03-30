e35213edf454758753cc17f08eff9689a239149f
bukharovev <bukharovev@gmail.com>
2019-03-30 23:30:45 +0300

    add nested tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,9 +2,16 @@ import fs from 'fs';
 import genDiff from '../src';
 
 const formatsFile = ['json', 'yaml', 'ini'];
-test.each(formatsFile)('%s diff', (format) => {
-  const before = `__tests__/__fixtures__/before.${format}`;
-  const after = `__tests__/__fixtures__/after.${format}`;
-  const result = fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8');
+test.each(formatsFile)('%s diff simple', (format) => {
+  const before = `__tests__/__fixtures__/__simple__/before.${format}`;
+  const after = `__tests__/__fixtures__/__simple__/after.${format}`;
+  const result = fs.readFileSync('__tests__/__fixtures__/__simple__/expected.txt', 'utf-8');
+  expect(genDiff(before, after)).toBe(result);
+});
+
+test.each(formatsFile)('%s diff nested', (format) => {
+  const before = `__tests__/__fixtures__/__nested__/before.${format}`;
+  const after = `__tests__/__fixtures__/__nested__/after.${format}`;
+  const result = fs.readFileSync('__tests__/__fixtures__/__nested__/expected.txt', 'utf-8');
   expect(genDiff(before, after)).toBe(result);
 });
