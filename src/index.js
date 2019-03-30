f8f843197dd26e5e59bbed9294b382b69321a596
bukharovev <bukharovev@gmail.com>
2019-03-30 22:20:14 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
-import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import parse from './parsers';
-
-const propertyTypes = [
-  {
-    check: ((obj1, obj2, key) => _.has(obj1, key) && _.has(obj2, key) && obj1[key] !== obj2[key]),
-    process: ((obj1, obj2, key) => `  + ${key}: ${obj2[key]}
-  - ${key}: ${obj1[key]}`),
-  },
-  {
-    check: ((obj1, obj2, key) => _.isEqual(obj1[key], obj2[key])),
-    process: ((obj1, obj2, key) => `    ${key}: ${obj1[key]}`),
-  },
-  {
-    check: ((obj1, obj2, key) => _.isUndefined(obj1[key])),
-    process: ((obj1, obj2, key) => `  + ${key}: ${obj2[key]}`),
-  },
-  {
-    check: ((obj1, obj2, key) => _.isUndefined(obj2[key])),
-    process: ((obj1, obj2, key) => `  - ${key}: ${obj1[key]}`),
-  },
-];
-
-const getPropertyTypes = (obj1, obj2, key) => (
-  propertyTypes.find(({ check }) => check(obj1, obj2, key)));
+import createAst from './createAst';
+import render from './render';
 
 const genDiff = (pathFile1, pathFile2) => {
   const dataFile1 = fs.readFileSync(pathFile1, 'utf-8');
@@ -36,12 +14,9 @@ const genDiff = (pathFile1, pathFile2) => {
   const obj1 = parse(dataFile1, formatFile1);
   const obj2 = parse(dataFile2, formatFile2);
 
-  const keys = _.union(_.keys(obj1), _.keys(obj2));
-  const result = keys.map((key) => {
-    const { process } = getPropertyTypes(obj1, obj2, key);
-    return process(obj1, obj2, key);
-  });
-  const output = `{\n${result.join('\n')}\n}`;
+  const ast = createAst(obj1, obj2);
+
+  const output = render(ast);
   return output;
 };
 
