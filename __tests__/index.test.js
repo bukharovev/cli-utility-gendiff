208dfc3774acf05e7ba4f35ad961d09d517e39d6
bukharovev <bukharovev@gmail.com>
2019-03-31 22:31:00 +0300

    test improvement

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,32 +1,30 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const formatsFile = ['json', 'yaml', 'ini'];
+const formatsFile = [
+  ['simple', 'standard', 'json'],
+  ['simple', 'standard', 'yaml'],
+  ['simple', 'standard', 'ini'],
+  ['simple', 'plain', 'json'],
+  ['simple', 'plain', 'yaml'],
+  ['simple', 'plain', 'ini'],
+  ['simple', 'json', 'json'],
+  ['simple', 'json', 'yaml'],
+  ['simple', 'json', 'ini'],
+  ['nested', 'standard', 'json'],
+  ['nested', 'standard', 'yaml'],
+  ['nested', 'standard', 'ini'],
+  ['nested', 'plain', 'json'],
+  ['nested', 'plain', 'yaml'],
+  ['nested', 'plain', 'json'],
+  ['nested', 'json', 'json'],
+  ['nested', 'json', 'yaml'],
+  ['nested', 'json', 'ini'],
+];
 
-test.each(formatsFile)('%s diff simple', (format) => {
-  const before = `__tests__/__fixtures__/__simple__/before.${format}`;
-  const after = `__tests__/__fixtures__/__simple__/after.${format}`;
-  const result = fs.readFileSync('__tests__/__fixtures__/__simple__/expected.txt', 'utf-8');
-  expect(genDiff(before, after)).toBe(result);
-});
-
-test.each(formatsFile)('%s diff nested format standard', (format) => {
-  const before = `__tests__/__fixtures__/__nested__/before.${format}`;
-  const after = `__tests__/__fixtures__/__nested__/after.${format}`;
-  const result = fs.readFileSync('__tests__/__fixtures__/__nested__/expected.txt', 'utf-8');
-  expect(genDiff(before, after)).toBe(result);
-});
-
-test.each(formatsFile)('%s diff nested format plain', (format) => {
-  const before = `__tests__/__fixtures__/__nested__/before.${format}`;
-  const after = `__tests__/__fixtures__/__nested__/after.${format}`;
-  const result = fs.readFileSync('__tests__/__fixtures__/__nested__/expectedPlain.txt', 'utf-8');
-  expect(genDiff(before, after, 'plain')).toBe(result);
-});
-
-test.each(formatsFile)('%s diff nested format json', (format) => {
-  const before = `__tests__/__fixtures__/__nested__/before.${format}`;
-  const after = `__tests__/__fixtures__/__nested__/after.${format}`;
-  const result = fs.readFileSync('__tests__/__fixtures__/__nested__/expectedJson.txt', 'utf-8');
-  expect(genDiff(before, after, 'json')).toBe(result);
+test.each(formatsFile)('%s diff %s %s', (folder, outputFormat, format) => {
+  const before = `__tests__/__fixtures__/__${folder}__/before.${format}`;
+  const after = `__tests__/__fixtures__/__${folder}__/after.${format}`;
+  const result = fs.readFileSync(`__tests__/__fixtures__/__${folder}__/${outputFormat}.txt`, 'utf-8');
+  expect(genDiff(before, after, `${outputFormat}`)).toBe(result);
 });
